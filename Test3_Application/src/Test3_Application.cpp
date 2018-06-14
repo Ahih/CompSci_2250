d1241010c89b9f48efe2b11fa841333610300f75
Anthony Huber-Winkler <ranthonywinkler@gmail.com>
2018-06-14 18:38:38 -0500

    Test3
    Commit to Other Branch

---
+++ Test3_Application/src/Test3_Application.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Test3_Application.cpp
+// Author      : Anthony Huber-Winkler
+// Version     :
+// Copyright   : Programming 2250
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Huh, weird..." << endl; // prints !!!Hello World!!!
+	return 0;
+}
