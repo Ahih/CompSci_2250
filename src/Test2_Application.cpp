50ceb5d3ac26c673ba3685acb10fd193f6f71683
Anthony Huber-Winkler <ranthonywinkler@gmail.com>
2018-06-14 18:30:22 -0500

    Test 2 Project
    Did this work?

---
+++ src/Test2_Application.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Test2_Application.cpp
+// Author      : Anthony Huber-Winkler
+// Version     :
+// Copyright   : Programming 2250
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Here WE GO AGAIN"<< endl;
+	return 0;
+}
