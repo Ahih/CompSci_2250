8ddc45bdbe78053e353267cede2f85250ac931d4
Anthony Huber-Winkler <ranthonywinkler@gmail.com>
2018-06-14 18:07:44 -0500

    Initial Commit
    Woo Hoo

---
+++ Test_Application/src/Test_Application.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Test_Application.cpp
+// Author      : Anthony Huber-Winkler
+// Version     :
+// Copyright   : Programming 2250
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
